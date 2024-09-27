634c53b91c603d79486fbf56d5701ec13542f111
RICH999999999 <geo-rich@mail.ru>
2024-09-27 19:31:50 +0300

    Update 1004.py

--- 1004.py
+++ 1004.py
@@ -1,16 +1,11 @@
 def find_pairs(n):
-    pairs = []
-    for i in range(1, 21):
-        for j in range(i, 21):
-            if (i + j) % n == 0:
-                pairs.append((i, j))
-    return pairs
-
-def generate_password(n):
-    pairs = find_pairs(n)
-    password = ''.join([str(i) + str(j) for i, j in pairs])
-    return password
-
-#Тест для числа от 3 до 20
-for n in range(3, 21):
-    print(f"Пароль для числа {n}: {generate_password(n)}")
+    result = ""
+#Перебираем первое число в паре от 1 до n-1
+    for i in range(1, n):
+#Перебираем второе число в паре от i до n-1
+        for j in range(i+1, n):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+n = int(input("Введите число от 3 до 20: "))
+print(f"Пароль для числа {n}: {find_pairs(n)}")
